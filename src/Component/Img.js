b270b5efcb58cbe5a45ca6351c10534bb46bfc7c
Gunswinger <gunswinger0@gmail.com>
2019-12-09 21:17:06 +0700

    updateWork

---
+++ src/Component/Img.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css';
+import '../style/Home.css';
+import img1 from '../Image/1.png';
+import img2 from '../Image/2.png';
+import img3 from '../Image/3.png';
+import img4 from '../Image/4.png';
+import img5 from '../Image/5.png';
+import img6 from '../Image/6.png';
+import img7 from '../Image/7.png';
+import img8 from '../Image/8.png';
+
+
+class Img extends React.Component
+{
+    render()
+    {
+        const params = {
+
+            effect: 'cube',
+            grabCursor: true,
+            cubeEffect: {
+                shadow: true,
+                slideShadows: true,
+                shadowOffset: 20,
+                shadowScale: 0.94,
+            },
+
+        }
+        return (
+
+            <Swiper {...params}>
+                <div className="swiper-slide"><a href="http://skyhouse-bsdcity.id/" target="_blank"><img src={img1} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="http://amigos.co.id/" target="_blank"><img src={img2} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/conakito/" target="_blank"><img src={img3} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/Cloning-Zalora-Frontpage/" target="_blank"><img src={img4} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/Babastudio-exercis-3/" target="_blank"><img src={img5} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/DevC-Exercise3/" target="_blank"><img src={img6} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/Todolist/" target="_blank"><img src={img7} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+                <div className="swiper-slide"><a href="https://sandikoprastyo.github.io/indiehouse" target="_blank"><img src={img8} alt="img" style={{ width: "300px", height: "300px" }} /></a></div>
+            </Swiper>
+
+        )
+    }
+}
+
+
+
+export default Img;
--- src/Component/Work.js
+++ src/Component/Work.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react';
 import '../style/Home.css';
-/* import Gallery from "react-photo-gallery"; */
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { photos } from "./photo";
+import Img from './Img';
+import 'swiper/css/swiper.css';
 
 
 class Work extends Component
 {
-    state = { modalIsOpen: false };
-    toggleModal = () =>
-    {
-        this.setState(state => ({ modalIsOpen: !state.modalIsOpen }));
-    };
     render()
     {
-        const { modalIsOpen } = this.state;
-
 
         return (
             <div className="App">
                 <div className="title">
                     <h1>Work</h1>
                 </div>
-                <ModalGateway>
-                    {modalIsOpen ? (
-                        <Modal onClose={this.toggleModal}>
-                            <Carousel views={photos} />
-                        </Modal>
-                    ) : null}
-                </ModalGateway>
+                <div className="swiper-container">
+                    <div class="swiper-wrapper">
+                        <Img />
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
             </div>
         );
     }
